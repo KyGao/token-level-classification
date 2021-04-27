9bbf60c968cb5e3943fd46c11400a5a0b62135bf
KyGao <im_gky@outlook.com>
2021-04-27 14:55:57 +0800

    0427: xlm15 token-level

--- train_udpos.sh
+++ train_udpos.sh
@@ -18,7 +18,7 @@ NUM_EPOCHS=$3
 LR=$4
 MAX_LENGTH=$5
 
-MODEL="xlm-mlm-100-1280"
+MODEL="xlm-mlm-xnli15-1024"
 TASK='udpos'
 LANGS='ar,bg,de,el,en,es,fr,hi,ru,th,tr,ur,vi,zh'
 
@@ -41,7 +41,7 @@ else
 fi
 
 DATA_DIR=$DATA_DIR/$TASK/${TASK}_processed_maxlen${MAX_LENGTH}/
-OUTPUT_DIR="$RESULT_DIR/$TASK/XLM100-LR${LR}-epoch${NUM_EPOCHS}-MaxLen${MAX_LENGTH}/"
+OUTPUT_DIR="$RESULT_DIR/$TASK/XLM15-LR${LR}-epoch${NUM_EPOCHS}-MaxLen${MAX_LENGTH}/"
 mkdir -p $OUTPUT_DIR
 CUDA_VISIBLE_DEVICES=0 python third_party/run_tag.py \
   --data_dir $DATA_DIR \
